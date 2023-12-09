6696caa4e305d3f30ea53e65d5e6d033ab241dcb
Jamie Thomas Grantham-Burton <jgranthamburton@Jamies-MacBook-Air>
2023-12-10 03:20:21 +1100

    create endpoint for create a new note with a post request

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,26 @@ const prisma = new PrismaClient();
 app.use(express.json());
 app.use(cors());
 
+// get notes
 app.get("/api/notes", async (req, res) => {
   const notes = await prisma.note.findMany();
   res.json(notes);
+});
+
+// create a new note
+app.post("/api/notes", async (req, res) => {
+  const { title, content } = req.body;
+  if (!title || !content) {
+    return res.status(400).send("title and content fields required");
+  }
+  try {
+    const note = await prisma.note.create({
+      data: { title, content },
+    });
+    res.json(note);
+  } catch (error) {
+    res.status(500).send("Oops something went wrong!");
+  }
 });
 
 app.listen(5001, () => {
